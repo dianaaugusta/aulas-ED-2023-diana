4ee28fe2593b0fcee87b443edf8a2a4a29637233
Diana Augusta <79462403+dianaaugusta@users.noreply.github.com>
2023-03-27 20:07:36 -0300

    FEAT adicionando exercicio de conceito da lista estatica INCOMPLETA

---
+++ lista-estatica-incompleta/src/main/java/org/example/ListaEstatica.java
@@ -0,0 +1,101 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class ListaEstatica {
+
+    // 01) Declarar vetor de int:
+    // É inicializado no construtor
+    private int[] vetor;
+
+    // 02) Criar atributo nroElem:
+    // Tem dupla função: representa quantos elementos foram adicionado no vetor
+    // Também o índice de onde será adicionado o próximo elemento
+    private int nroElem;
+
+    // 03) Criar Construtor:
+    // Recebe como argumento o tamanho máximo do vetor
+    // Cria vetor com tamanho máximo informado
+    // Inicializa nroElem
+    public ListaEstatica(int tam) {
+        this.vetor = new int[tam];
+        this.nroElem = 0;
+    }
+
+    // 04) Método adiciona:
+    // Recebe o elemento a ser adicionado na lista
+    // Se a lista estiver cheia usar IllegalStateException();
+    public void adiciona(int elemento){
+        if(vetor.length == nroElem){
+            throw new IllegalStateException("Lista Cheia");
+        }
+
+        for (int i = 0 ; i < vetor.length ; i++){
+            vetor[i] = elemento;
+        }
+    }
+
+    // 05) Método busca:
+    // Recebe o elemento a ser procurado na lista
+    // Retorna o índice do elemento, se for encontrado
+    // Retorna -1 se não encontrou
+    public int busca(int nro){
+        int inicio = 0;
+        int fim = vetor.length - 1;
+        int meio = (inicio + fim)/2;
+
+        for(var i=0; i < fim; i++){
+            if(nro == vetor[meio]){
+                return meio;
+            }
+            if(nro > vetor[meio]){
+                inicio = meio +1;
+            }else{
+                fim = meio-1;
+            }
+        }
+
+        return -1;
+
+    }
+
+    // 06) Método removePeloIndice:
+    // Recebe o índice do elemento a ser removido
+    // Se o índice for inválido, retorna false
+    // Se removeu, retorna true
+    public boolean removePeloIndice(int elementoPraRemover){
+        if(elementoPraRemover < 0 || elementoPraRemover >= nroElem){
+            return false;
+        }
+
+        for(var i= elementoPraRemover; i < nroElem-1; i++){
+            vetor[i] = vetor[i + 1];
+        }
+
+        nroElem--;
+        return true;
+    }
+
+
+
+    // 07) Método removeElemento
+    // Recebe um elemento a ser removido
+    // Utiliza os métodos busca e removePeloIndice
+    // Retorna false, se não encontrou o elemento
+    // Retorna true, se encontrou e removeu o elemento
+
+
+    // 08 Método exibe:
+    // Exibe os elementos da lista
+
+
+    //Métodos getVetor e getNroElem
+    //São usados nos testes
+    public int getNroElem() {
+        return nroElem;
+    }
+
+    public int[] getVetor() {
+        return vetor;
+    }
+}
